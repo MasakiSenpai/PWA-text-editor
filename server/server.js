46e3643affa7f728ebc25e97bf4c688cc9e05181
MasakiDesktop <mandij50@gmail.com>
2023-03-09 14:48:08 -0500

    repush server with movedown

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 require('./routes/htmlRoutes')(app);
 
