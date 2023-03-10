7065296bb464a751209176fa342cabe80e87b185
MasakiDesktop <mandij50@gmail.com>
2023-03-10 10:50:52 -0500

    wepback fix?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,9 +56,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: [
-                '@babel/plugin-preposal-object-rest-spread', '@babel/transform-runtime'
-              ]
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
